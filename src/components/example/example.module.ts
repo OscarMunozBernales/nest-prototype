5a3801355ebbcadfa6d70500753dea68c614f7da
Oscar <munoz.bernales.oscar@gmail.com>
2024-11-07 21:34:47 -0300

    PERF: Se eliminaron los archivos .gitkeep que no son necesarios

--- src/components/example/example.module.ts
+++ src/components/example/example.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ExampleController } from './example.controller';
+import { HttpRequestModule } from '@services/index';
 
 @Module({
-  controllers: [ExampleController]
+  controllers: [ExampleController],
+  imports: [
+    HttpRequestModule
+  ],
 })
 export class ExampleModule {}
